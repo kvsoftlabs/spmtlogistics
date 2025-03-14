6362ef3a4d42bf4826cbe2e0aac61321cb48c2d5
Vivek Somasundaram <viveksomasundaram@Viveks-MacBook-Air.local>
2025-03-14 22:17:47 +0530

    Updated env

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'pmtlogis_root',
+        'password'     => 'tywker-suqWiw-xerdi2',
+        'database'     => 'pmtlogis_pmt_logistics',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
